42a232b0fd11458492082a2789393a4776ff012e
Markus Ressel <mail@markusressel.de>
2022-01-18 07:56:10 +0100

    v1.1.0

--- cmd/version.go
+++ cmd/version.go
@@ -9,7 +9,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of this program",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("1.0.0")
+		fmt.Printf("1.1.0")
 	},
 }
 
